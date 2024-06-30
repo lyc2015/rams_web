c558dabd031484f286799da613f42dcfa3989617
tianyuanyyy <109130123+tianyuanyyy@users.noreply.github.com>
2024-06-30 20:46:36 +0900

    Update store.js
    客户信息登陆页面的redux的store需要和整体的store文件和一下！！

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,3 +4,19 @@ import reducer from "./reducer";
 const store = createStore(reducer);
 
 export default store;
+
+
+//お客様登録ページのstore設定
+//import { createStore, applyMiddleware, combineReducers } from 'redux';
+// import thunk from 'redux-thunk';
+// import dataReducer  from '../redux/customerRegister/reducers';
+// import reducer from "./reducer";
+
+
+// const rootReducer = combineReducers({
+//   data: dataReducer,
+// });
+
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+
+// export default store;
